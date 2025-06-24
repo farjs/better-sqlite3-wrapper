d994a536b75d75dfe8a4e6b00c9f5be0e75af7f0
Viktor Podzigun <viktor.podzigun@gmail.com>
2025-06-24 19:37:18 +0200

    Use node:sqlite on node version >= 22

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
-
 [![CI](https://github.com/farjs/better-sqlite3-wrapper/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/farjs/better-sqlite3-wrapper/actions/workflows/ci.yml?query=workflow%3Aci+branch%3Amain)
 [![npm version](https://img.shields.io/npm/v/@farjs/better-sqlite3-wrapper)](https://www.npmjs.com/package/@farjs/better-sqlite3-wrapper)
 
 ## @farjs/better-sqlite3-wrapper
 
-Thin api wrapper around [better-sqlite3](https://github.com/WiseLibs/better-sqlite3)
-and [bun:sqlite](https://bun.sh/docs/api/sqlite) to allow cross- runtime/engine usage.
+Thin api wrapper around:
+
+- [better-sqlite3](https://github.com/WiseLibs/better-sqlite3) on node version < 22
+- [node:sqlite](https://nodejs.org/docs/latest-v22.x/api/sqlite.html) on node version >= 22
+- and [bun:sqlite](https://bun.sh/docs/api/sqlite)
+
+to allow cross- runtime/engine usage.
 
 It exposes only [a subset](./index.d.ts) of `better-sqlite3` api because not everything
-is available in `bun:sqlite`.
+is available in `bun:sqlite`/`node:sqlite`.
 
 For example, since `run` returns `undefined` you can use the following queries to get
- `changes` and/or `lastInsertRowid`:
+`changes` and/or `lastInsertRowid`:
 
 ```javascript
 const changes = db.prepare("SELECT changes() AS changes;").get().changes;
-const lastInsertRowId = db.prepare("SELECT last_insert_rowid() AS id;").get().id;
+const lastInsertRowId = db
+  .prepare("SELECT last_insert_rowid() AS id;")
+  .get().id;
 ```
 
 Also, since `db.pragma` is not available in `bun:sqlite`, you can run:
--- index.js
+++ index.js
@@ -3,7 +3,70 @@ const wrapper = function () {
     return require("bun:sqlite").Database;
   }
 
-  return require("better-sqlite3");
+  if (isModuleAvailable("better-sqlite3")) {
+    return require("better-sqlite3");
+  }
+
+  const { DatabaseSync } = require("node:sqlite");
+  function dbCtor(path, options) {
+    if (options) {
+      return nodeDbWrapper(new DatabaseSync(path, options));
+    }
+
+    return nodeDbWrapper(new DatabaseSync(path));
+  }
+  return dbCtor;
 };
+
+function isModuleAvailable(name) {
+  try {
+    require.resolve(name);
+    return true;
+  } catch {}
+
+  return false;
+}
+
+function nodeDbWrapper(db) {
+  //check if node supports it
+  //see: https://github.com/nodejs/node/issues/57431
+  if (db.transaction) {
+    return db;
+  }
+
+  //see: https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md#transactionfunction---function
+  db.transaction = (f) => {
+    const doTx = (...args) => {
+      try {
+        const res = f(...args);
+        db.prepare("COMMIT").run();
+        return res;
+      } catch (error) {
+        db.prepare("ROLLBACK").run();
+        throw error;
+      }
+    };
+
+    const txFn = (...args) => {
+      db.prepare("BEGIN").run();
+      return doTx(...args);
+    };
+    txFn.deferred = (...args) => {
+      db.prepare("BEGIN DEFERRED").run();
+      return doTx(...args);
+    };
+    txFn.immediate = (...args) => {
+      db.prepare("BEGIN IMMEDIATE").run();
+      return doTx(...args);
+    };
+    txFn.exclusive = (...args) => {
+      db.prepare("BEGIN EXCLUSIVE").run();
+      return doTx(...args);
+    };
+
+    return txFn;
+  };
+  return db;
+}
 
 module.exports = wrapper();
