a79da46a0a3824406b982ea6e0af4504229d4ca2
Viktor Podzigun <viktor.podzigun@gmail.com>
2025-06-25 20:19:01 +0300

    Use node:sqlite on node version >= 22 (#3)

--- index.test.js
+++ index.test.js
@@ -14,12 +14,17 @@ it("should open in-memory database", () => {
   const query = db.prepare("select 'Hello world' as message;");
 
   //then
-  assert.deepEqual(query.get(), { message: "Hello world" });
+  assert.deepEqual({ ...query.get() }, { message: "Hello world" });
 
   //when & then
   const results = query.all();
   assert.deepEqual(Array.isArray(results), true);
-  assert.deepEqual(results, [{ message: "Hello world" }]);
+  assert.deepEqual(
+    results.map((r) => {
+      return { ...r };
+    }),
+    [{ message: "Hello world" }]
+  );
 
   //when & then
   db.close();
@@ -62,19 +67,73 @@ it("should create file-based database", () => {
   const results = db.prepare("select * from test order by id;").all();
 
   //then
-  assert.deepEqual(results, [
-    {
-      id: 1,
-      name: "test1",
-    },
-    {
-      id: 2,
-      name: "test2",
-    },
-  ]);
+  assert.deepEqual(
+    results.map((r) => {
+      return { ...r };
+    }),
+    [
+      {
+        id: 1,
+        name: "test1",
+      },
+      {
+        id: 2,
+        name: "test2",
+      },
+    ]
+  );
 
   //cleanup
   db.close();
   fs.unlinkSync(file);
   fs.rmdirSync(tmpDir);
+});
+
+it("should rollback failed transaction", () => {
+  //when
+  const db = new Database(":memory:");
+  const changesQuery = db.prepare("SELECT changes() AS changes;");
+  const lastInsertRowIdQuery = db.prepare("SELECT last_insert_rowid() AS id;");
+  db.prepare(
+    `
+          create table test(
+              id     integer primary key,
+              name   text not null
+          );
+      `
+  ).run();
+  assert.deepEqual(changesQuery.get().changes, 0);
+  assert.deepEqual(lastInsertRowIdQuery.get().id, 0);
+
+  const error = Error("test error");
+  const txFn = db.transaction((name) => {
+    const insert = db.prepare("insert into test (name) values (?);");
+    insert.run(name);
+    throw error;
+  });
+  let caughtError = null;
+
+  //when
+  try {
+    txFn("test1");
+  } catch (err) {
+    caughtError = err;
+  }
+
+  //then
+  assert.deepEqual(caughtError === error, true);
+  assert.deepEqual(changesQuery.get().changes, 1);
+  assert.deepEqual(lastInsertRowIdQuery.get().id, 1);
+
+  //when & then
+  const query = db.prepare("select id, name from test;");
+  assert.deepEqual(
+    query.all().map((r) => {
+      return { ...r };
+    }),
+    []
+  );
+
+  //cleanup
+  db.close();
 });
